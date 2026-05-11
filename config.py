71a2fb696ab238edb72d04a929f16f256f338bee
Bakir Chaban <B.Chaban@campus.lmu.de>
2026-05-11 18:38:16 +0200

    [update] configurations and env update

---
+++ config.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+dfs = pd.read_csv("/Users/kingmopser/BachelorThesis/BachelorsThesisCode/task_metadata_tabarena51.csv")
+
+columns = ["QSAR_fish_toxicity","QSAR-TID-11","wine_quality","healthcare_insurance_expenses","Another-Dataset-on-used-Fiat-50","miami_housing"]
