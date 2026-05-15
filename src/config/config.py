eac785ea15ada282a6b156b6270e7f9925282338
Bakir Chaban <B.Chaban@campus.lmu.de>
2026-05-15 18:24:28 +0200

    [feat] Metrics and Config Implemented

---
+++ src/config/config.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+metadatapath = "/Users/kingmopser/BachelorThesis/BachelorsThesisCode/src/data/task_metadata_tabarena51.csv"
+#columns = ["QSAR_fish_toxicity","QSAR-TID-11","wine_quality","healthcare_insurance_expenses","Another-Dataset-on-used-Fiat-50","miami_housing"]
+columns = ["wine_quality","miami_housing"]
