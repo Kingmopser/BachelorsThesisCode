11b94c113cbf8184b391901b3aa2a5f7912df9b5
Bakir Chaban <B.Chaban@campus.lmu.de>
2026-05-16 16:45:02 +0200

    [feature] Robustness Test implemented

--- src/main.py
+++ src/main.py
@@ -13,6 +13,9 @@ from pathlib import Path
 from PreProcessing.PreProcessing import LoadData,PreProcessing
 from config.metrics import GaussianNll, WinklerScore
 import optuna
+from functools import partial
+from mlflow.tracking import MlflowClient
+import ast
 
 ROOT = Path(__file__).resolve().parent
 
@@ -21,46 +24,21 @@ MLFLOW_DB = ROOT / "data" / "mlflow.db"
 MLFLOW_ARTIFACT_ROOT = ROOT / "mlruns"
 MLFLOW_DB.parent.mkdir(parents=True, exist_ok=True)
 MLFLOW_ARTIFACT_ROOT.mkdir(parents=True, exist_ok=True)
-
 mlflow.set_tracking_uri(f"sqlite:///{MLFLOW_DB}")
 
-experiment_name = "test"
 
-if mlflow.get_experiment_by_name(experiment_name) is None:
-    mlflow.create_experiment(
-        experiment_name,
-        artifact_location=(MLFLOW_ARTIFACT_ROOT / experiment_name).as_uri(),
-    )
 
-mlflow.set_experiment(experiment_name)
-mlflow.autolog()
-
-#load data : miami_housing, wine_quality ; #TODO: no preprocessing
-datasets = LoadData()
-seeds = [1,8,16]
-#preprocess
-Rng_Ho_Split = 12
-
-def objective(trial):
+def objective(trial,X_train,X_test,y_train,y_test):
             
             with mlflow.start_run(nested=True,run_name=f"trial_{trial.number}") as child_run:
+                
                 #define params
-                bde_hidden_layers = trial.suggest_categorical("hidden_layers",[[16,16],[32,32]
-                                                                               #,[16,16,16],[32,32,32]
-                                                                               ])
                 #bde_n_members =trial.suggest_int("n_members",4,10,step =1)
-                bde_desired_energy_var_start, bde_desired_energy_var_end = trial.suggest_categorical("var_start_end",[(0.5,0.1),
-                                                                                                                      (0.05,0.01),
-                                                                                                                      #(0.005,0.001),
-                                                                                                                      #(0.0005,0.0001)
-                                                                                                                      ])
-                bde_warmup_steps, bde_n_samples = trial.suggest_categorical("warmup_steps_n_samples",[(1000,200),
-                                                                                                      #(2000,400),
-                                                                                                      #(3000,600),
-                                                                                                      #(5000,1000)
-                                                                                                      ])
-                epochs = 30
-                validation_split = 0.15
+                bde_hidden_layers = trial.suggest_categorical("hidden_layers",[[16,16],[32,32],[16,16,16],[32,32,32]])
+                bde_desired_energy_var_start, bde_desired_energy_var_end = trial.suggest_categorical("var_start_end",[(0.5,0.1),(0.05,0.01),(0.005,0.001),(0.0005,0.0001)])
+                bde_warmup_steps, bde_n_samples = trial.suggest_categorical("warmup_steps_n_samples",[(1000,200),(2000,400),(3000,600),(5000,1000) ])                                                                                         
+                epochs = 100
+                validation_split = 0.15 # or 0.0 damnnnn ahahah
                 patience = 10
                 
                 params = {
@@ -82,7 +60,6 @@ def objective(trial):
                 
                 '''
                 performance on inner Test data 
-                
                 #main metrics: RMSE, Winkler Score, Coverage, NLL 
                 '''
                 y_pred = regressor.predict(X_test)
@@ -101,38 +78,124 @@ def objective(trial):
                     "Winkler_Coverage": float(coverage),
                     "Negative_log_likelihood": float(nll)
                 })
-                mlflow.sklearn.log_model(regressor,name="model")
+                mlflow.sklearn.log_model(regressor,name=f"model_trial_{trial.number}")
                 trial.set_user_attr("run_id",child_run.info.run_id)
                 
                 return float(meanWinkler)
                 #basically, after we found best config, lets say 50 runs, 
 
+def extractParams(paramdict):
+    
+    params = {
+                    "hidden_layers": ast.literal_eval(paramdict.get("hidden_layers")),
+                    "desired_energy_var_start": float(paramdict.get("desired_energy_var_start")),
+                    "desired_energy_var_end": float(paramdict.get("desired_energy_var_end")),
+                    "warmup_steps":int(paramdict.get("warmup_steps")),
+                    "n_samples":int(paramdict.get( "n_samples")),
+                    "seed": int(paramdict.get("seed")),
+                    "epochs":int(paramdict.get("epochs")),
+                    "validation_split": float(paramdict.get("validation_split")),
+                    "patience":int(paramdict.get("patience"))
+                }
+    return params
+
+def RobustTest(X_train_HO, X_test_HO, y_train_HO, y_test_HO ,seeds,run_name):
+    '''
+    RobustTest would be a seperate run. So that we can log the performance of the best model. Ideally in Optuna we would have per dataset 1 HPO Study Experiment, and 1 Stresstest 
+    '''
+    client = MlflowClient(tracking_uri=f"sqlite:///{MLFLOW_DB}")
+    experiment = client.get_experiment_by_name("Experiment_Dataset:miami_housing")
+    
+    runs = client.search_runs(experiment_ids=[experiment.experiment_id],
+                              filter_string=f"attributes.run_name = {run_name}",
+                                order_by=["attributes.start_time DESC"],
+                                max_results=1,
+        )
+    
+    parent_run_id = runs[0].info.run_id
+    best_child_run_id = client.get_run(parent_run_id).data.params["best_child_run_id"]        
+    best_child_params = extractParams(client.get_run(best_child_run_id).data.params)
+    
+    with mlflow.start_run(run_name=f"Robustnesstest_Data_{'miami_housing'}") as run:
+        for i,seed in enumerate(seeds): 
+            with mlflow.start_run(nested=True,run_name=f"test_{i}") as child_run:   
+                X_train,_,y_train,_ = train_test_split(X_train_HO,y_train_HO,random_state=seed,train_size=0.8)
+                
+                #TODO: this is what need to be modular
+                
+                #initialize bde regressor with params and fit on train data
+                regressor = BdeRegressor(**best_child_params)
+                regressor.fit(X_train,y_train)
+                
+                #evaluate performance on test dataset
+                y_pred = regressor.predict(X_test_HO)
+                
+                #rmse
+                rmse = root_mean_squared_error(y_true=y_test_HO.ravel(),y_pred=y_pred)
+            
+                #winklerscore, coverage
+                _, intervals = regressor.predict(X_test_HO, credible_intervals=[0.05, 0.95]) # get 90% credible interval
+                meanWinkler, coverage = WinklerScore(y_test_HO.ravel(),pi_lower=intervals[0],pi_upper=intervals[1], returnCoverage=True)
+            
+                #nll
+                mu,sigma = regressor.predict(X_test_HO,mean_and_std=True)
+                nll = GaussianNll(y_test_HO.ravel(),mu=mu,sigma=sigma)
+            
+                mlflow.log_metrics({
+                    "RMSE": float(rmse),
+                    "Mean_Winkler_Score": float(meanWinkler),
+                    "Winkler_Coverage": float(coverage),
+                    "Negative_log_likelihood": float(nll)
+                })
 
 
 
+#load data : miami_housing, wine_quality ; #TODO: no preprocessing
+datasets = LoadData()
+seeds = [24,38,136]
+#preprocess
+Rng_Ho_Split = 12
 
+'''
+imagine for loop 
+'''
 
+experiment_name = f"Experiment_Dataset:{'miami_housing'}" # would be dataset name obv
 
+if mlflow.get_experiment_by_name(experiment_name) is None:
+    mlflow.create_experiment(
+        experiment_name,
+        artifact_location=(MLFLOW_ARTIFACT_ROOT / experiment_name).as_uri(),
+    )
+
+mlflow.set_experiment(experiment_name)
+mlflow.autolog()
+
+#this would happen for each dataset
 X_train_HO, X_test_HO, y_train_HO, y_test_HO, yScaler = PreProcessing("miami_housing",data=datasets["miami_housing"],random_seed=Rng_Ho_Split) # we do 1 hold-out-split
+
 # split train in to test and val
 X_train, X_test,y_train,y_test = train_test_split(X_train_HO,y_train_HO,random_state=Rng_Ho_Split,train_size=0.8)
-        #hpo
-        #create study
-        #initialize child ruins
-        #set main seed for sklearn. 
         
-with mlflow.start_run(run_name="first_hpo") as run:
+with mlflow.start_run(run_name=f"HPO_Study_{'miami_housing'}") as run:
     
-    n_trials = 4
+    n_trials = 50
+    
     mlflow.log_param("n_trials",n_trials)
     
     study = optuna.create_study(direction="minimize")
-    study.optimize(objective,n_trials=n_trials)
+    
+    obj = partial(objective,X_train = X_train,X_test = X_test,y_train=y_train,y_test=y_test)
+    
+    # Continue even if individual trials fail (they will be marked as FAIL)
+    study.optimize(obj, n_trials=n_trials, catch=(Exception,))
     
     mlflow.log_params(study.best_params)
+    
     mlflow.log_metrics({"best_winkler":study.best_value})
     
     if best_run_id := study.best_trial.user_attrs.get("run_id"):
         mlflow.log_param("best_child_run_id",best_run_id)    
                 
-                
+RobustTest(X_train_HO=X_train_HO,y_train_HO=y_train_HO,X_test_HO=X_test_HO,y_test_HO=y_test_HO, seeds=seeds, run_name=f"HPO_Study_{'miami_housing'}")
+    
